63bcfcb4815503e6035340dd87aa534c1e017c93
Gio <gmpowers774@gmail.com>
2026-04-21 18:48:07 -0400

    Modal Focus

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,14 @@
 /* eslint-disable */
-
+let body = document.querySelector("body");
 let modal = document .querySelector(".modal-content");
 let openModal = document.getElementById("open-modal");
 let modalMaker = document.createElement("img");
+let modalBacking = document.createElement("div");
+modalBacking.setAttribute("class", "modal-background");
+modalBacking.setAttribute("style", "opacity:0%;")
 modalMaker.setAttribute("src", "MAXIMIZE-YOUR-MODALS.png");
 modalMaker.setAttribute("style", "opacity:0%;");
+body.appendChild(modalBacking);
 modal.appendChild(modalMaker);
 
 let menuOpener = document.getElementById("open-menu");
@@ -19,10 +23,14 @@ openModal.onclick = (event) => {
     modalTracker = modalTracker+1;
     modalMaker.removeAttribute("style");
     modalMaker.setAttribute("style", "opacity:100%;");
+    modalBacking.removeAttribute("style");
+    modalBacking.setAttribute("style", "opacity:100%;")
   }else if(modalTracker >= 1){
     modalTracker = modalTracker-1;
     modalMaker.removeAttribute("style");
     modalMaker.setAttribute("style", "opacity:0%;");
+    modalBacking.removeAttribute("style");
+    modalBacking.setAttribute("style", "opacity:0%;")
   }
 };
 
